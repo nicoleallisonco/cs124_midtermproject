679da620184a4258610f5df2a5cd1acc14e0144b
nicoleallisonco <allison.co@obf.ateneo.edu>
2020-03-14 18:57:13 +0800

    Added getters/setters/toString and Created annotations

---
+++ src/annotations/Column.java
@@ -0,0 +1,14 @@
+package annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE_USE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Column {
+	public String name();
+	public String sqlType();
+	public boolean id() default false;
+}
--- src/entity/Student.java
+++ src/entity/Student.java
@@ -18,6 +18,43 @@ public class Student {
 	@Column(name="age", 		sqlType="INTEGER")
 	private Integer age;
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", first=" + first + ", last=" + last + ", age=" + age + "]";
+	}
+
 	
 	// make all the getter/setter/toString
 
